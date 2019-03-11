db9d5a2e8515d6519051d8993a7acc907ad34073
Вадим Шванов <adm.shwan@gmail.com>
2019-03-11 21:00:27 +0300

    Добавлен установщик

--- CodeBlogFitness.BL/Controller/ControllerBase.cs
+++ CodeBlogFitness.BL/Controller/ControllerBase.cs
@@ -5,7 +5,7 @@ namespace CodeBlogFitness.BL.Controller
 {
     public abstract class ControllerBase
     {
-        private readonly IDataSaver manager = new DatabaseSaver();
+        private readonly IDataSaver manager = new SerializableSaver();
 
         protected void Save<T>(List<T> item) where T : class
         {
